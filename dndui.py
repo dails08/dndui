8a8a54c7bd7dd9049a22c459d1917c88359b29e2
Chris Dailey <christopher.dailey@gmail.com>
2023-09-14 11:35:57 -0400

    Functional Ship function.  Added avatar_size and {left, right}_buffer variables.

--- dndui.py
+++ dndui.py
@@ -27,9 +27,6 @@ def on_connect(client, userdata, flags, rc):
     print("Subscribing to topic","dnd")
     print(client.subscribe("dnd"))
     
-def pull_image_from_clipboard():
-    return im
-
 
 
 class LogWindow(tk.Toplevel):
@@ -50,22 +47,101 @@ class InitiativeWindow(tk.Toplevel):
         self.title("Initiative Display")
         
         self.render_list = []
-        
-        def InitiativeSet():
-            pass
+        self.initiative_group_list = []
         
         self.upper_frame = ttk.Frame(self)
         self.lower_frame = ttk.Frame(self)
         
-        #tmp_canvas = tk.Canvas(upper_frame, bg = "#0000FF", width = 300, height = 300)
-        #tmp_canvas.pack()
         
         self.initiative_list = tk.Listbox(self.upper_frame)
-        #self.initiative_list.grid(row = 0, column = 0, sticky = "ns")
         self.initiative_list.place(x = 0, y = 0, height = 300, width = 400)
         
+        avatar_size = (200,200)
+        upper_buffer = 10
+        left_buffer = 10
         
-        def parse_dndb():
+        class InitiativeGroup():
+            def __init__(self, name = "Nobody", avatar = None, canvas = None, location = [0,0]):
+                self.name = name
+                self.avatar = avatar
+                self.location = location
+                self.destination = self.location
+                self.canvas = canvas
+                self.avatar_image_be = ImageTk.PhotoImage(self.avatar.resize(avatar_size))
+                self.avatar_image_id = self.canvas.create_image(left_buffer + self.location[0], upper_buffer + self.location[1], image = self.avatar_image_be, anchor = "nw")
+                
+                self.font = tkFont.Font(family='Arial', size=12, weight='bold')
+                self.name_id = self.canvas.create_text(left_buffer + self.location[0] + avatar_size[0]/2,upper_buffer + self.location[1] + avatar_size[1] + 10, text = self.name, width = 200, font = self.font, justify = tk.CENTER)
+
+                self.anim = 0
+            
+            def destroy(self):
+                logger.debug("Destroying " + self.name)
+                self.canvas.delete(self.avatar_image_id)
+                self.canvas.delete(self.name_id)
+                
+                
+            def setDestination(self, coords):
+                self.destination = coords
+            def setDestinationAndMove(self, coords):
+                self.destination = coords
+                self.updateLocation()
+                
+            def getCurrentPosition(self):
+                return self.location
+            def getDestination(self):
+                return self.destination
+                
+            def redraw(self):
+                self.canvas.coords(self.avatar_image_id, 
+                    left_buffer + self.location[0], 
+                    upper_buffer + self.location[1])
+                self.canvas.coords(self.name_id, 
+                    left_buffer + self.location[0] + avatar_size[0]/2, 
+                    upper_buffer + self.location[1] + avatar_size[1] + 10)
+
+                
+            def snapToDestination(self):
+                self.location = self.destination
+                self.canvas.coords(self.avatar_image_id, left_buffer + self.location[0], upper_buffer + self.location[1])
+                self.canvas.coords(self.name_id, 
+                    left_buffer + self.location[0] + avatar_size[0]/2, 
+                    upper_buffer + self.location[1] + avatar_size[1] + 10)
+                if self.anim != 0:
+                    root.after_cancel(self.anim)
+                    self.anim = 0
+
+            def updateLocation(self, steps=100, duration = 200):
+                if self.anim != 0:
+                    root.after_cancel(self.anim)
+                    self.anim = 0
+                if self.getCurrentPosition() != self.getDestination():
+                    #print("Position needs updating!")
+                    # calculate y path
+                    # hard-code animation pattern for now
+                    x_distance = self.getDestination()[0] - self.getCurrentPosition()[0]
+                    x_path = self.getCurrentPosition()[0] + x_distance * np.array([pt.easeOutQuint(x) for x in np.linspace(0,1, steps+1)])
+                    per_step = int(duration/steps)
+                    # call update_step fcn
+                    self.updateLocationStep(x_path, per_step)
+
+            def updateLocationStep(self, x_path, per_step):
+                #print("Taking a step!")
+                if len(x_path) == 0:
+                    #print("Done moving!")
+                    self.snapToDestination()
+                else:
+                    self.location = (x_path[0], self.location[1])
+                    self.redraw()
+                    #self.canvas.coords(self.avatar_image_id, self.getDestination()[0], x_path[0])
+                    #self.canvas.coords(self.text_block_id, self.destination_position[0]-300, y_path[0])
+                    #self.canvas.coords(self.text_words_id, self.destination_position[0]-150, y_path[0]+50)
+                    
+                    
+                    self.anim = root.after(per_step, lambda: self.updateLocationStep(x_path[1:], per_step))
+
+        
+        def parseDNDB():
             logger.debug("Parsing dndb content")
             avatar_imgs = []
             render_list = []
@@ -73,9 +149,7 @@ class InitiativeWindow(tk.Toplevel):
             logger.debug("Looking for avatars...")
             # Find the combat cards
             html = pyperclip.paste()
-            # print(html)
-            soup = BeautifulSoup(html)
-            # avatars_list = soup.find_all("div", class_ = "combatant-summary__avatar")
+            soup = BeautifulSoup(html, features = "html.parser")
             avatars_list = soup.find_all("div", class_ = "combatant-card__mid-bit combatant-summary")
             
             
@@ -83,12 +157,12 @@ class InitiativeWindow(tk.Toplevel):
             for avatar in avatars_list:
                 content = requests.get(avatar.img['src']).content
                 image_data = BytesIO(content)
-                img = ImageTk.PhotoImage(Image.open(image_data).resize((250,250)))
+                img = Image.open(image_data)
                 avatar_imgs.append(img)
                 
                 avatar_name = avatar.find_all("div", class_ = "combatant-summary__name")[0].string
                 render_list.append((img, avatar_name))
-                logger.debug("Parsed 1.")
+                logger.debug("Parsed " + avatar_name)
             self.render_list = render_list
             logger.debug("Set internal render list")
             self.initiative_list.delete(0, self.initiative_list.size()-1)
@@ -96,10 +170,21 @@ class InitiativeWindow(tk.Toplevel):
                 self.initiative_list.insert(tk.END, elem[1])
             logger.debug("Set new initiative list contents.")
             
-        pull_initiative_btn = ttk.Button(self.upper_frame, text = "Pull Order", command = parse_dndb)
+        pull_initiative_btn = ttk.Button(self.upper_frame, text = "Pull Order", command = parseDNDB)
         pull_initiative_btn.place(x = 410, y = 0)
         
-        ship_btn = ttk.Button(self.upper_frame, text = "Ship")
+        def shipFcn():
+            for elem in self.initiative_group_list:
+                elem.destroy()
+            self.initiative_group_list = []
+            for elem in self.render_list:
+                logger.debug(elem[1])
+                self.initiative_group_list.append(InitiativeGroup(name = elem[1], avatar = elem[0], canvas = self.display_canvas, location = [0,0]))
+            for i in range(len(self.initiative_group_list)):
+                self.initiative_group_list[i].setDestinationAndMove(coords = (i * 300, 0))
+                
+        
+        ship_btn = ttk.Button(self.upper_frame, text = "Ship", command = shipFcn)
         ship_btn.place(x = 410, y = 40)
 
         drop_btn = ttk.Button(self.upper_frame, text = "Drop")
@@ -116,7 +201,7 @@ class InitiativeWindow(tk.Toplevel):
         self.prep_name = ttk.Entry(self.upper_frame, text = "Name")
         self.prep_name.place(x = 1500, y = 0, width = 250)
         
-        def pull_pic_fcn():
+        def pullPicFcn(self):
             im = ImageGrab.grabclipboard()
             logger.debug("Pulled from clipboard")
             if im:
@@ -124,7 +209,7 @@ class InitiativeWindow(tk.Toplevel):
                 self.prep_img_cv = self.prep_canvas.create_image(0,0, image = self.prep_img_be, anchor = "nw")
 
         
-        self.pull_pic_btn = ttk.Button(self.upper_frame, text = "Pull Pic", command = pull_pic_fcn)
+        self.pull_pic_btn = ttk.Button(self.upper_frame, text = "Pull Pic", command = pullPicFcn)
         self.pull_pic_btn.place(x = 1500, y = 30, anchor = "ne", width = 100)
         
         
